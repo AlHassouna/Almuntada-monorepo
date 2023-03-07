a04a8eaec8657825e3f37a251f7f0a8332f52ff7
hhasona <hhasona@gmail.com>
2023-03-07 15:29:55 +0400

    fix: yarn with nx

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Al-Manshaah Project')
+    .setDescription('Using swagger to manage all the endpoints')
+    .setVersion('1.0')
+    .addTag('Al-Manshaah')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
   const port = process.env.PORT || 8000;
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
