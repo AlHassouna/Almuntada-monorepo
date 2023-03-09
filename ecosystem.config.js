61069f010a9a9b240f0fe05a78843b5e37a1579d
Hassan Hassouna <hhasona@gmail.com>
2023-03-09 16:13:06 +0400

    deploy to ec2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'workspace',
-      script: 'apps/server/main.js',
+      script: 'dist/apps/server/main.js',
       env: {
         PORT: 8000,
         NODE_ENV: 'production',
