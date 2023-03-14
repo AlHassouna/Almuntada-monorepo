7608676539fb684a1485701f4d2f709aa301f5cc
Hassan Hassouna <hhasona@gmail.com>
2023-03-14 23:50:13 +0400

    feat: event table

--- apps/server/src/app/events/dto/create-event.dto.ts
+++ apps/server/src/app/events/dto/create-event.dto.ts
@@ -1,21 +1,37 @@
-import { ApiProperty } from '@nestjs/swagger';
-import {IsArray, IsNotEmpty} from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
+import {IsDate, IsNotEmpty} from 'class-validator';
+
 export class CreateEventDto {
-
-  @ApiProperty(
-    {
-      description: 'The name of the event'
-    }
-  )
+  @ApiProperty({
+    description: 'The name of the event',
+  })
   @IsNotEmpty()
-  name:string
+  name: string;
 
-@ApiProperty(
-  {
-    description: 'The organizer of the event'
-  }
-)
-@IsNotEmpty()
-        @IsArray()
-organizer:string
+  @ApiProperty({
+    description: 'The organizer of the event',
+  })
+  @IsNotEmpty()
+  organizer: string;
+
+  @ApiProperty({
+    description: 'The location of the event',
+  })
+  @IsNotEmpty()
+  location: string;
+
+  @ApiProperty({
+    description: 'The information of the event',
+  })
+  @IsNotEmpty()
+  information: string;
+
+
+  @ApiProperty({
+    description: 'The date of the event',
+  })
+
+  @IsDate()
+  dateOfEvent: Date
+
 }
