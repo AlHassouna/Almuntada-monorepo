34a8500cb8f55ee3cecfbbfca0310e3b71fd60ac
Hassan Hassouna <hhasona@gmail.com>
2023-04-04 14:17:43 +0400

    feat: admin dashboard & fixing

--- apps/client/pages/academic/index.tsx
+++ apps/client/pages/academic/index.tsx
@@ -76,7 +76,7 @@ const Academic: FC = () => {
           variants={fadeIn("up", "tween", 0.2, 1)}
           className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-[orange] mb-4"
         >
-          {intl.messages["academicpage.text"]}
+          {intl.formatMessage({id: "academicpage.text"})}
         </motion.p>
         <div className="flex flex-col sm:flex-row mx-auto w-full sm:justify-around items-center h-full">
           <AutoComplete
