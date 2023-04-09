0739cff35c380b9aef443fa17577b1978d0dab5a
Hassan Hassouna <hhasona@gmail.com>
2023-04-09 21:59:46 +0400

    fix: building

--- libs/system-design/src/lib/API/api.ts
+++ libs/system-design/src/lib/API/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import {environment} from './environment/environment';
 
 const version = 'v1';
+
+console.log("environment.apiUrl", environment.apiUrl);
 export const backendInstance = axios.create({
   // .. configure axios baseURL
   baseURL: `${environment.apiUrl}/${version}`,
