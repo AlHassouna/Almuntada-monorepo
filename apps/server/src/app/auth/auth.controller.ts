59577c9d33d355fbe99e51c19e8d1ed4477f4669
eliassalom <elias.29@hotmail.com>
2023-04-11 21:57:58 +0400

    feat: add encryption to the phone number

--- apps/server/src/app/auth/auth.controller.ts
+++ apps/server/src/app/auth/auth.controller.ts
@@ -1,10 +1,10 @@
-import {Body, Controller, Post, Res} from "@nestjs/common";
-import {AuthService} from "./auth.service";
-import {UserInputDto} from "./dto/users.dto";
-import {ApiTags} from "@nestjs/swagger";
-import {LoginDto} from "./dto/auth.dto";
-import {Response} from "express";
-import {JwtService} from "@nestjs/jwt";
+import { Body, Controller, Get, Post, Query, Res } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { UserInputDto } from "./dto/users.dto";
+import { ApiTags } from "@nestjs/swagger";
+import { LoginDto } from "./dto/auth.dto";
+import { Response } from "express";
+import { JwtService } from "@nestjs/jwt";
 
 @Controller("auth")
 @ApiTags("auth")
@@ -17,8 +17,8 @@ export class AuthController {
     return this.authService.signUp(user);
   }
 
-  @Post('login')
-  LogIn(@Body() data: LoginDto, @Res({passthrough: true}) res: Response) {
+  @Get()
+  LogIn(@Query() data: LoginDto, @Res({ passthrough: true }) res: Response) {
     return this.authService.login(data, res);
   }
 }
