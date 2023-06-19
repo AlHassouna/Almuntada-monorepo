dfa1b4477331736c812d2ae76186458a6df4a679
Hassan Hassouna <hhasona@gmail.com>
2023-06-19 14:30:52 +0400

    fix: cors & swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,22 +8,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(requestIp.mw());
   app.enableCors({
-    origin: true,
+    origin: 'https://almuntada.org',
     credentials: true,
   });
   const globalPrefix = 'api';
   const version = 'v1'
 
-  app.setGlobalPrefix(`${globalPrefix}/${version}`);
-  const config = new DocumentBuilder()
-    .setTitle('Al-Manshaah Project')
-    .setDescription('Using swagger to manage all the endpoints')
-    .setVersion('1.0')
-    .addTag('Al-Manshaah')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  // app.setGlobalPrefix(`${globalPrefix}/${version}`);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Al-Manshaah Project')
+  //   .setDescription('Using swagger to manage all the endpoints')
+  //   .setVersion('1.0')
+  //   .addTag('Al-Manshaah')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
   const port = 8000;
-  SwaggerModule.setup(`api/${version}`, app, document);
+  // SwaggerModule.setup(`api/${version}`, app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}/${version}`
