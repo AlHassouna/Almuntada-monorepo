fbbd0a9896bdd9161c8b4e76fad68fb0bf320621
Hassan Hassouna <hhasona@gmail.com>
2023-12-03 08:53:37 +0200

    fix: date of event

--- apps/client/components/Card/EventCard.tsx
+++ apps/client/components/Card/EventCard.tsx
@@ -5,63 +5,64 @@ import {AcademicCardBox} from "./card.styled";
 import CardMedia from "@mui/material/CardMedia";
 
 interface Props {
-    name: string;
-    information: string;
-    location: string;
-    organizer: string;
-    dateOfEvent: string;
-    image: string;
+  name: string;
+  information: string;
+  location: string;
+  organizer: string;
+  dateOfEvent: string;
+  image: string;
 }
 
 
 export const EventCard: FC<Props> = ({
-                                         name,
-                                         information,
-                                         location,
-                                         organizer,
-                                         dateOfEvent,
-                                         image
+                                       name,
+                                       information,
+                                       location,
+                                       organizer,
+                                       dateOfEvent,
+                                       image
                                      }) => {
 
-    return (
-        <RevealWrapper delay={100} duration={2000} reset={true}>
-            <AcademicCardBox>
-                <CardMedia
-                    sx={{
-                        height: 300,
-                        filter: "grayscale(100%)",
-                        "&:hover": {
-                            transition: "all 0.5s ease",
-                            filter: "grayscale(0%)"
-                        }
-                    }}
-                    image={image}
-                    title={name}
-                />
-                <Typography sx={{textAlign: "center"}} gutterBottom variant="h5" component="div">
-                    {`${name}`}
-                </Typography>
-                <Typography sx={{
-                    textAlign: "center"
-                }
-                } variant="subtitle1" color="text.secondary">
-                    {` ${information}`}
-                </Typography>
-                <Typography sx={{
-                    textAlign: "center"
-                }
-                } variant="subtitle1" color="text.secondary">
-                    {`${location}`}
-                </Typography>
-                <Typography
-                    sx={{
-                        textAlign: "center"
-                    }}
-                    variant="body2"
-                    color="text.secondary"
-                >
-                </Typography>
-            </AcademicCardBox>
-        </RevealWrapper>
-    );
+  return (
+    <RevealWrapper delay={100} duration={2000} reset={true}>
+      <AcademicCardBox>
+        <CardMedia
+          sx={{
+            height: 300,
+            filter: "grayscale(100%)",
+            "&:hover": {
+              transition: "all 0.5s ease",
+              filter: "grayscale(0%)"
+            }
+          }}
+          image={image}
+          title={name}
+        />
+        <Typography sx={{textAlign: "center"}} gutterBottom variant="h5" component="div">
+          {`${name}`}
+        </Typography>
+        <Typography sx={{
+          textAlign: "center"
+        }
+        } variant="subtitle1" color="text.secondary">
+          {` ${information}`}
+        </Typography>
+        <Typography sx={{
+          textAlign: "center"
+        }
+        } variant="subtitle1" color="text.secondary">
+          {`${location}`}
+        </Typography>
+        <Typography
+          sx={{
+            textAlign: "center"
+          }}
+          variant="body2"
+          color="text.secondary"
+        >
+          {dateOfEvent}
+        </Typography>
+      </AcademicCardBox>
+    </RevealWrapper>
+  );
 };
